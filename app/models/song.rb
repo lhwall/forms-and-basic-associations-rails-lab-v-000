fe8d5b9ade8e6ef1b9e9a314117190eff61844d1
Lucy Wall <lhwall@gmail.com>
2018-07-13 11:51:26 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -23,7 +23,7 @@ class Song < ActiveRecord::Base
     notes.each do |content|
       if content.strip != ""
         byebug
-      self.notes << Note.new(:content => content[:content])
+      self.notes << Note.new(:content => content)
     end
   end
 end
